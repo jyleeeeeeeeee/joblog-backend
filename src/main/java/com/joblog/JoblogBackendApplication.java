8a5029238c76b393cb1a8cbb11857b69ab632fd4
jylee <jyl9311@gmail.com>
2025-06-26 20:28:50 +0900

    init : 프로젝트 초기 생성

---
+++ src/main/java/com/joblog/JoblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoblogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joblog/JoblogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
