f2c3c4277968073f697da9aa7394da99c869f241
jylee <jyl9311@gmail.com>
2025-07-16 20:58:14 +0900

    asdfasdfasdf

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-echo "🧹 1. Spring Boot 전체 빌드 및 테스트 실행"
-./gradlew clean build --no-build-cache --rerun-tasks
+echo "🧪 0. 테스트 실행"
+./gradlew clean test
 
 if [ $? -ne 0 ]; then
-  echo "❌ 테스트 또는 빌드 실패. 배포 중단."
+  echo "❌ 테스트 실패. 배포 중단."
   exit 1
 fi
 
-echo "🧨 2. 기존 Docker 컨테이너 정리"
-docker-compose down --remove-orphans
-docker rm -f joblog-app joblog-mysql joblog-redis joblog-jenkins 2>/dev/null || true
-docker container prune -f
+echo "🧹 1. Spring Boot 빌드"
+./gradlew bootJar
 
-echo "🐳 3. Docker Compose로 재시작"
+
+echo "🐳 2. Docker Compose 재시작"
+docker-compose down
 docker-compose up -d --build
 
+#echo "🧨 2. 기존 Docker 컨테이너 정리"
+#docker-compose down --remove-orphans
+#docker rm -f joblog-app joblog-mysql joblog-redis joblog-jenkins 2>/dev/null || true
+#docker container prune -f
+#echo "🐳 3. Docker Compose로 재시작"
+#docker-compose up -d --build
+
 echo "✅ 배포 완료"
+
+
+
+
+
+
