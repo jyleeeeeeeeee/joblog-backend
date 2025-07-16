632c5a335d5bfa32eb301f0eadbf8d6ac4bc2c2f
jylee <jyl9311@gmail.com>
2025-07-16 20:34:28 +0900

    test

--- src/test/java/com/joblog/SampleTest.java
+++ src/test/java/com/joblog/SampleTest.java
@@ -1,14 +1,13 @@
-// 📁 src/test/java/com/joblog/SampleTest.java
-
 package com.joblog;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SampleTest {
+
     @Test
     void failThisTest() {
         System.out.println("🔥 테스트 실행됨");
-        fail("일부러 실패시킴");
+        fail("일부러 실패");
     }
 }
