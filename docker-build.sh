03139f130e5c8b00450e359d231e80ad19040fcc
jylee <jyl9311@gmail.com>
2025-07-21 07:19:04 +0900

    zcxv

--- docker-build.sh
+++ docker-build.sh
@@ -8,8 +8,8 @@ load_env
 
 # 컨테이너 제거 (Jenkins 제외)
 echo "🧹 Redis/MySQL/App 컨테이너 제거 (Jenkins 제외)"
-docker rm -f joblog-redis joblog-mysql joblog-app joblog-jenkins 2>/dev/null
-
+#docker rm -f joblog-redis joblog-mysql joblog-app joblog-jenkins 2>/dev/null
+docker-compose --env-file .env.docker down
 wait_for_redis
 wait_for_db
 #run_tests
@@ -17,7 +17,7 @@ run_build
 
 # 컨테이너 재실행
 echo "🐳 Docker Compose 재실행 (Jenkins 제외)"
-docker-compose --env-file .env.docker up -d --build joblog-redis joblog-mysql joblog-app
-
+#docker-compose --env-file .env.docker up -d --build joblog-redis joblog-mysql joblog-app
+docker-compose --env-file .env.docker up -d --build
 echo "🎉 Jenkins 빌드 배포 완료"
 
