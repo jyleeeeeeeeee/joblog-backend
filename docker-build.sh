01c6721fac41228fe95f8278979d0090d93b4f31
jylee <jyl9311@gmail.com>
2025-07-22 04:47:08 +0900

    sgfdg

--- docker-build.sh
+++ docker-build.sh
@@ -86,15 +86,18 @@ export $(grep -v '^#' "$ENV_FILE" | xargs)
 echo "🧪 프로필 설정 : ${SPRING_PROFILES_ACTIVE}"
 
 echo "🧼 [local-build.sh] 로컬 전체 초기화 및 컨테이너 재빌드 시작"
-
+docker ps
+sleep 5
 # 🔥 모든 컨테이너 및 네트워크 제거 (Jenkins 포함)
 echo "🧹 모든 컨테이너 및 네트워크 제거"
 docker-compose --env-file "$ENV_FILE" -f docker-compose.yml down
-
+docker ps
+sleep 5
 # ✅ 전체 컨테이너 재생성 (Jenkins 포함)
 echo "🐳 전체 컨테이너 재생성"
 docker-compose --env-file "$ENV_FILE" up -d --build
-
+docker ps
+sleep 5
 check_redis
 check_mysql
 build_no_test
