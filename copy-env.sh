3c148f17a964aebfe15a59e8e7602ef55f16b877
jylee <jyl9311@gmail.com>
2025-08-01 04:47:32 +0900

    scp env file

--- copy-env.sh
+++ copy-env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# ✅ Jenkins 빌드 전에 실행: .env.dev를 Jenkins 컨테이너 workspace로 복사
-echo "📦 .env.dev Jenkins 컨테이너로 복사"
+# ✅ Jenkins 빌드 전에 실행: .env.* 파일들을 Jenkins 컨테이너로 복사
 
-docker cp .env.dev .env.stagig .env.prod joblog-jenkins:/var/jenkins_home/workspace/joblog
+echo "📦 .env.dev → Jenkins 컨테이너로 복사"
+docker cp .env.dev joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.dev
+
+echo "📦 .env.staging → Jenkins 컨테이너로 복사"
+docker cp .env.staging joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.staging
+
+echo "📦 .env.prod → Jenkins 컨테이너로 복사"
+docker cp .env.prod joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.prod
