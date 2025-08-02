5fcad4c666aa2e1546f69fbc4c86a8e47f0fc8be
jylee <jyl9311@gmail.com>
2025-08-03 01:26:06 +0900

    주석 수정

--- ec2-to-container.sh
+++ ec2-to-container.sh
@@ -2,9 +2,11 @@
 set -e
 echo "📦 Jenkins 컨테이너로 파일 복사 중..."
 
-docker exec joblog-jenkins rm -f /var/jenkins_home/workspace/joblog/.env.dev
-docker exec joblog-jenkins rm -f /var/jenkins_home/workspace/joblog/.env.staging
-docker exec joblog-jenkins rm -f /var/jenkins_home/workspace/joblog/.env.prod
+docker exec -it joblog-jenkins mkdir -p /var/jenkins_home/.ssh
+
+docker cp ~/joblog-key.pem joblog-jenkins:/var/jenkins_home/.ssh/joblog-key.pem
+docker exec -it joblog-jenkins chmod 600 /var/jenkins_home/.ssh/joblog-key.pem
+
 docker cp ~/.env.dev joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.dev
 docker cp ~/.env.staging joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.staging
 docker cp ~/.env.prod joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.prod
--- local-to-jenkins.sh
+++ local-to-jenkins.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 📦 전송할 파일들
-FILES=".env.dev .env.staging .env.prod docker-compose.yml"
+FILES="joblog-key.pem .env.dev .env.staging .env.prod docker-compose.yml"
 
 echo "🚚 로컬 → Jenkins EC2로 env 및 compose 파일 전송..."
 scp -i "joblog-key.pem" $FILES "ubuntu@3.37.2.69:~/"
