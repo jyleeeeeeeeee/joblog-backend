9ef46f3fc5def658f641be7798a273a2c7c31028
jylee <jyl9311@gmail.com>
2025-08-03 03:17:15 +0900

    error update

--- copy-env.sh
+++ copy-env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # ✅ Jenkins 빌드 전에 실행: .env.* 파일들을 Jenkins 컨테이너로 복사
 
+ls
 echo "📦 .env.dev → Jenkins 컨테이너로 복사"
-docker cp .env.dev joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.dev
+docker cp ./env/.env.dev joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.dev
 
 echo "📦 .env.staging → Jenkins 컨테이너로 복사"
-docker cp .env.staging joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.staging
+docker cp ./env/.env.staging joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.staging
 
 echo "📦 .env.prod → Jenkins 컨테이너로 복사"
-docker cp .env.prod joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.prod
+docker cp ./env/.env.prod joblog-jenkins:/var/jenkins_home/workspace/joblog/.env.prod
